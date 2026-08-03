24d9f367eb7bc7fe63d0b162a3d8d07872473d30
barakvalley108-afk <barakvalley108@gmail.com>
2026-08-03 19:21:26 +0530

    Redirect authenticated panel users to their assigned panel

--- app/panel-login/page.tsx
+++ app/panel-login/page.tsx
@@ -1,8 +1,12 @@
+import { redirect } from "next/navigation";
+import { getPanelSession, panelRoute } from "../panel-auth";
 import LoginForm from "./login-form";
 import "./panel-login.css";
 
 export const dynamic = "force-dynamic";
 
 export default async function PanelLogin() {
+  const session = await getPanelSession();
+  if (session) redirect(panelRoute(session));
   return <LoginForm />;
 }
