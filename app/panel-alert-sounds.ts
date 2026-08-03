0206e814081bfa2328849708f337e3de872c314f
barakvalley108-afk <barakvalley108@gmail.com>
2026-08-03 17:02:15 +0530

    Use reliable deployed panel alert sounds

--- app/panel-alert-sounds.ts
+++ app/panel-alert-sounds.ts
@@ -1,4 +1,4 @@
-export const NEW_ORDER_SOUND = "/virus_calling.mp3";
-export const ORDER_CANCELLED_SOUND = "/Order-up-bell-sound.mp3";
+export const NEW_ORDER_SOUND = "/order-success.mp3";
+export const ORDER_CANCELLED_SOUND = "/order-failed.mp3";
 export const NEW_ORDER_FALLBACK_SOUND = "/order-success.mp3";
 export const ORDER_CANCELLED_FALLBACK_SOUND = "/order-failed.mp3";
