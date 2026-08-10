6ca328602599848edaaafee52f5f9ca5126e5056
barakvalley108-afk <barakvalley108@gmail.com>
2026-08-10 13:46:30 +0530

    fix: strengthen owner and founder structured data

--- components/founder/FounderSchema.tsx
+++ components/founder/FounderSchema.tsx
@@ -1,37 +1,49 @@
 export default function FounderSchema() {
   const schema = {
     "@context": "https://schema.org",
-    "@type": "Person",
-    "@id": "https://sabkadelivery.in/#karan-nath",
-    name: "Karan Nath",
-    jobTitle: "Founder",
-    description:
-      "Karan Nath is a founder of Sabka Delivery. He co-founded the company with Prem Kumar Nath.",
-    image: "https://sabkadelivery.in/founder/karan.jpg",
-    worksFor: {
-      "@type": "Organization",
-      "@id": "https://sabkadelivery.in/#organization",
-      name: "Sabka Delivery",
-    },
-    alumniOf: {
-      "@type": "School",
-      name: "Primrose English Medium Senior Secondary School, Lala",
-    },
-    address: {
-      "@type": "PostalAddress",
-      addressLocality: "Lala Bazar",
-      addressRegion: "Assam",
-      addressCountry: "India",
-    },
-    url: "https://sabkadelivery.in/founder",
+    "@graph": [
+      {
+        "@type": ["Organization", "DeliveryService"],
+        "@id": "https://sabkadelivery.in/#organization",
+        name: "Sabka Delivery",
+        url: "https://sabkadelivery.in/",
+        description:
+          "Sabka Delivery is owned by Prem Kumar Nath and founded by Prem Kumar Nath and Karan Nath.",
+        owner: { "@id": "https://sabkadelivery.in/#prem-kumar-nath" },
+        founder: [
+          { "@id": "https://sabkadelivery.in/#prem-kumar-nath" },
+          { "@id": "https://sabkadelivery.in/#karan-nath" },
+        ],
+      },
+      {
+        "@type": "Person",
+        "@id": "https://sabkadelivery.in/#prem-kumar-nath",
+        name: "Prem Kumar Nath",
+        jobTitle: "Owner and Founder",
+        description:
+          "Prem Kumar Nath is the owner and one of the founders of Sabka Delivery.",
+        image: "https://sabkadelivery.in/founder/prem.png",
+        worksFor: { "@id": "https://sabkadelivery.in/#organization" },
+        url: "https://sabkadelivery.in/about",
+      },
+      {
+        "@type": "Person",
+        "@id": "https://sabkadelivery.in/#karan-nath",
+        name: "Karan Nath",
+        jobTitle: "Founder",
+        description:
+          "Karan Nath is one of the founders of Sabka Delivery.",
+        image: "https://sabkadelivery.in/founder/karan.jpg",
+        worksFor: { "@id": "https://sabkadelivery.in/#organization" },
+        url: "https://sabkadelivery.in/founder",
+      },
+    ],
   };
 
   return (
     <script
       type="application/ld+json"
-      dangerouslySetInnerHTML={{
-        __html: JSON.stringify(schema),
-      }}
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
     />
   );
 }
